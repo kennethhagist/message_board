58ad6f98412cbab17a1263d43640b4bf7c45c012
kkh <khagist@hollandamerica.com>
2018-06-05 15:40:30 -0700

    message board assignment

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded());
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+app.get("/", function(request, response) {
+    Message.find({}, false, true).populate('_comments').exec(function(err, messages) {
+        response.render('index.ejs', { messages: messages });
+    });
+});
+
+app.post("/message", function(request, response) {
+    var newMessage = new Message({ name: request.body.name, message: request.body.message });
+    newMessage.save(function(err) {
+        if (err) {
+            console.log(err);
+            response.render('index.ejs', { errors: newMessage.errors });
+        } else {
+            console.log("success");
+            response.redirect('/');
+        }
+    });
+});
+
+app.post("/comment/:id", function(request, response) {
+    const messageId = request.params.id;
+    Message.findOne({ _id: messageId }, function(err, message) {
+        const newComment = new Comment({ name: request.body.name, text: request.body.comment });
+        newComment._message = message._id;
+        Message.update({ _id: message._id }, { $push: { _comments: newComment }}, function(err) {
+
+        });
+        newComment.save(function(err) {
+            if (err) {
+                console.log(err);
+                response.render('index.ejs', { errors: newComment.errors });
+            } else {
+                console.log("comment added");
+                response.redirect("/");
+            }
+        });
+    });
+});
+
+app.listen(8000, function() {
+    console.log("server running on port 8000");
+});
+
+mongoose.connect('mongodb://127.0.0.1/message_board', function(err, db) {
+    if (err) {
+        console.log("error here");
+        console.log(err);
+    }
+});
+
+const Schema = mongoose.Schema;
+const MessageSchema = new mongoose.Schema({
+    name: String,
+    message: String,
+    _comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
+});
+
+MessageSchema.path('name').required(true, 'Name cannot be blank');
+MessageSchema.path('message').required(true, 'Message cannot be blank');
+mongoose.model("Message", MessageSchema);
+
+const Message = mongoose.model("Message");
+const CommentSchema = new mongoose.Schema({
+    name: String,
+    text: String,
+    _message: {type: Schema.Types.ObjectId, ref: 'Message'}
+});
+
+CommentSchema.path('name').required(true, 'Name cannot be blank');
+CommentSchema.path('text').required(true, 'Comment cannot be blank');
+mongoose.model("Comment", CommentSchema);
+
+const Comment = mongoose.model("Comment");
